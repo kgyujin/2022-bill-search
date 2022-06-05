ea2de850c268dade43c27fd44b2b00d18a4121e8
kgyujin <k_gyujin@daum.net>
2022-06-05 19:12:43 +0900

    Update bill
    html css config

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var searchText = document.getElementById("text").value;
+
+function searchResult(){
+    location.href ="bill-list.html";
+    console.log("JS test★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★");
+    document.getElementById("textResult").value = searchText;
+}
